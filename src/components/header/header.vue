bacb92ce1f712470e80874cc50410943fcda136e
swarty <swarty16@gmail.com>
2022-06-13 20:53:24 +0300

    update structure

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,27 +1,42 @@
 <template>
-  <div>
-    hello world {{ data }}
-    <button @click="increeseData">
-      increese data
-    </button>
-  </div>
+  <header class="cc-header">
+    <div class="cc-container">
+      <div class="cc-header__container">
+        <div class="cc-header__header">
+          <h1 class="cc-header__title cc-header-1">
+            Check list application
+          </h1>
+          <p class="cc-header__subtitle">
+            Application with help whitch you can made checklist for any goals and follow by this.
+          </p>
+        </div>
+        <div class="cc-header__body">
+          <CcProjectForm />
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
-  ref,
 } from 'vue';
+import CcProjectForm from '@/components/project-form/project-form.vue';
 
 export default defineComponent({
   name: 'cc-header',
+  components: {
+    CcProjectForm,
+  },
 });
 </script>
 
 <script lang="ts" setup>
-const data = ref(0);
-
-function increeseData () {
-  data.value += 1;
-}
 </script>
+
+<style lang="scss" scoped>
+  @import '@/assets/style/components/container.scss';
+  @import '@/assets/style/components/typography.scss';
+  @import './header.scss';
+</style>
---
+++ src/components/project-form/project-form.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="cc-project-form">
+    sdsd
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cc-project-form',
+};
+</script>
